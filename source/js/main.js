022430eb03933b5087ce09733509bad972b63066
Dmitry <bigvolkovski@gmail.com>
2021-04-08 18:19:49 +0300

    Стилизация модалки и правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,39 @@
 'use strict';
 
+document.querySelectorAll(`input[name=phone]`).forEach((e) => {
+  IMask(e, {mask: `+{7}(000)000-00-00`});
+});
+
 const footerMenu = document.querySelectorAll(`.page-footer__list`);
 const footerNavTitle = document.querySelectorAll(`.page-footer__nav`);
+
+const modalWindow = document.querySelector(`.modal`);
+const modalOverlay = modalWindow.querySelector(`.modal__overlay`);
+const modalClosed = modalWindow.querySelector(`.modal__close`);
+const modalOpened = document.querySelector(`.header-nav__button`);
+
+if (modalWindow !== null) {
+  modalOpened.addEventListener(`click`, () => {
+    modalWindow.classList.add(`modal--active`);
+  });
+
+  modalClosed.addEventListener(`click`, () => {
+    modalWindow.classList.remove(`modal--active`);
+  });
+
+  modalOverlay.addEventListener(`click`, () => {
+    modalWindow.classList.remove(`modal--active`);
+  });
+
+  window.addEventListener(`keydown`, function (evt) {
+    if (evt.keyCode === 27) {
+      if (modalWindow.classList.contains(`modal--active`)) {
+        evt.preventDefault();
+        modalWindow.classList.remove(`modal--active`);
+      }
+    }
+  });
+}
 
 if (footerMenu !== null && footerNavTitle !== null) {
   Array.from(footerMenu).forEach((el) => {
